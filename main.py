3e05699a2e8bd383fa6b3af82311b4822a372abc
Mauricio Hernandez (ITSS10) <mauricio.hernandez@genstudents.org>
2026-09-11 15:11:38 -0500

    Calculadora mejor

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-def main():
-  print("Hello learners!")
+def addmultiplenumbers(numbers):
+    total = 0.0
 
-if __name__=="__main__":
-  main()
+    for number in numbers:
+        total = total + number
+
+    return total
+
+
+def multiplymultiplenumbers(numbers):
+    total = 1.0
+
+    for number in numbers:
+        total = total * number
+
+    return total
+
+
+def isitaninteger(num):
+    return num % 1 == 0
+
+
+def isiteven(num):
+    if isitaninteger(num) and num % 2 == 0:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    main()
